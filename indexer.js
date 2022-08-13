0271ab594bd47727ccdfeecd79860cbe157f359e
liul <48413031+liulaz@users.noreply.github.com>
2022-08-13 14:18:50 +0300

    Added query compatibility for indexer and weighter

--- indexer.js
+++ indexer.js
@@ -5,73 +5,100 @@ import rmvStopwrd from "./stopword_remover.js";
 
 const indexData = {};
 
-function indexTerms(filesArray, outputIndexFilePath) {
-  indexData["corpus_size"] = filesArray.length;
-  indexData["corpus_word_count"] = {};
-  indexData["words"] = {};
+function indexTerms(corpus, outputIndexFilePath, type) {
+  if (type === "doc") {
+    // Documnets
+    const filesArray = corpus;
+    indexData["corpus_size"] = filesArray.length;
+    indexData["corpus_word_count"] = {};
+    indexData["words"] = {};
 
-  const indexPath = outputIndexFilePath + "/indexFile.json";
+    const indexPath = outputIndexFilePath + "/docIndexFile.json";
 
-  filesArray.forEach((filePath) => {
-    try {
-      // read files
-      const fileContents = fs.readFileSync(filePath, "utf8");
+    filesArray.forEach((filePath) => {
+      try {
+        // read files
+        const fileContents = fs.readFileSync(filePath, "utf8");
 
-      indexData["corpus_word_count"][filePath] = fileContents.length;
+        indexData["corpus_word_count"][filePath] =
+          fileContents.split(" ").length;
 
-      // preprocess
-      const unStemmedWords = rmvStopwrd(lexAnalyze(fileContents)).split(" ");
-      const stemmedWords = unStemmedWords.map((word) => stem(word));
-      const result = stemmedWords
-        .filter((e) => e)
-        .filter((e) => {
-          return e.length > 1;
+        // preprocess
+        const unStemmedWords = rmvStopwrd(lexAnalyze(fileContents)).split(" ");
+        const stemmedWords = unStemmedWords.map((word) => stem(word));
+        const result = stemmedWords
+          .filter((e) => e)
+          .filter((e) => {
+            return e.length > 1;
+          });
+
+        // index
+        let wordFlag = 0;
+        result.forEach((word) => {
+          if (word in indexData["words"]) {
+            indexData.words[word].forEach((pathObj) => {
+              if (filePath in pathObj) {
+                pathObj[filePath]++;
+                wordFlag = 1;
+              }
+            });
+            if (wordFlag === 0) {
+              indexData.words[word].push({ [filePath]: 1 });
+            } else {
+              wordFlag = 0;
+            }
+          } else {
+            indexData.words[word] = [{ [filePath]: 1 }];
+          }
         });
 
-      // index
-      let wordFlag = 0;
-      result.forEach((word) => {
-        if (word in indexData["words"]) {
-          indexData.words[word].forEach((pathObj) => {
-            if (filePath in pathObj) {
-              pathObj[filePath]++;
-              wordFlag = 1;
-            }
-          });
-          if (wordFlag === 0) {
-            indexData.words[word].push({ [filePath]: 1 });
-          } else {
-            wordFlag = 0;
-          }
-        } else {
-          indexData.words[word] = [{ [filePath]: 1 }];
+        const jsonString = JSON.stringify(indexData, null, 2);
+
+        try {
+          fs.writeFileSync(indexPath, jsonString);
+          console.log(`Contents of ${filePath} successfully added to index`);
+        } catch (error) {
+          console.log("Index creation failed", error);
         }
+      } catch (error) {
+        console.log(`Error reading ${filePath} file from disk:`, error);
+      }
+    });
+  } else if (type === "query") {
+    // Query
+    indexData["corpus_size"] = 1;
+    indexData["corpus_word_count"] = corpus.split(" ").length;
+    indexData["words"] = {};
+
+    const indexPath = outputIndexFilePath + "/queryIndexFile.json";
+
+    // preprocess
+    const unStemmedWords = rmvStopwrd(lexAnalyze(corpus)).split(" ");
+    const stemmedWords = unStemmedWords.map((word) => stem(word));
+    const result = stemmedWords
+      .filter((e) => e)
+      .filter((e) => {
+        return e.length > 1;
       });
 
-      const jsonString = JSON.stringify(indexData, null, 2);
-
-      try {
-        fs.writeFileSync(indexPath, jsonString);
-        console.log(`Contents of ${filePath} successfully added to index`);
-      } catch (error) {
-        console.log("Index creation failed", error);
+    // index
+    result.forEach((word) => {
+      if (word in indexData["words"]) {
+        indexData.words[word]++;
+      } else {
+        indexData.words[word] = 1;
       }
+    });
+
+    const jsonString = JSON.stringify(indexData, null, 2);
+
+    try {
+      fs.writeFileSync(indexPath, jsonString);
+      console.log(`Contents of Query successfully added to index`);
     } catch (error) {
-      console.log(`Error reading ${filePath} file from disk:`, error);
+      console.log("Index creation failed", error);
     }
-  });
+  }
 }
-
-const files = [
-  "C:\\Users\\user\\Desktop\\information_retrieval_system\\felig-toolkit\\corpus\\ከድሉ ባሻገር በኢትዮጵያ ቀጣይ ዕጣ ፈንታ ላይ ትኩረቱን ያደረገው የአትሌቶች አቀባበል.txt",
-  "C:\\Users\\user\\Desktop\\information_retrieval_system\\felig-toolkit\\corpus\\የአፍሪካ ዋንጫ ማጣሪያ ቀሪ የምድብ ጨዋታ ተራዘመ.txt",
-  "C:\\Users\\user\\Desktop\\information_retrieval_system\\felig-toolkit\\corpus\\የኢትዮጵያ አትሌቲክስ ቡድን ዛሬ አዲስ አበባ ይገባል.txt",
-  "C:\\Users\\user\\Desktop\\information_retrieval_system\\felig-toolkit\\corpus\\የፕሪሚየር ሊጉ የቀጣይ ዓመት ጨዋታ መርሐ ግብር ይፋ ተደረገ.txt",
-];
-
-indexTerms(
-  files,
-  "C:\\Users\\user\\Desktop\\information_retrieval_system\\felig-toolkit"
-);
 
 export default indexTerms;
--- term_weighter.js
+++ term_weighter.js
@@ -1,47 +1,73 @@
 import fs from "fs";
 
-function weigh_terms(indexFilePath, outputWeightedTermsPath) {
+function weigh_terms(indexFilePath, outputWeightedTermsPath, typeOfIndex) {
   const weightedTerms = {};
-  const weightedTermsPath = outputWeightedTermsPath + "/weightedTermsFile.json";
+  const weightedTermsPath =
+    outputWeightedTermsPath + `/${typeOfIndex}WeightedTermsFile.json`;
 
   // read index file
   try {
     const jsonString = fs.readFileSync(indexFilePath, "utf8");
-    const dataset = JSON.parse(jsonString);
-
     try {
-      let tf = 0;
-      let idf = 0;
-      let tf_idf = 0;
+      const dataset = JSON.parse(jsonString);
+      if (typeOfIndex === "doc") {
+        let tf = 0;
+        let idf = 0;
+        let tf_idf = 0;
 
-      // calculate length normalized term  frequency and inverse document frewuency
-      Object.keys(dataset.words).forEach((word) => {
-        idf = Math.log2(dataset.corpus_size / dataset.words[word].length);
+        // calculate length normalized term  frequency and inverse document frewuency
+        Object.keys(dataset.words).forEach((word) => {
+          idf = Math.log2(dataset.corpus_size / dataset.words[word].length);
 
-        dataset.words[word].forEach((filePathObj) => {
-          let file = Object.keys(filePathObj)[0];
-          let freq = Object.values(filePathObj)[0];
+          dataset.words[word].forEach((filePathObj) => {
+            let file = Object.keys(filePathObj)[0];
+            let freq = Object.values(filePathObj)[0];
 
-          tf = freq / dataset.corpus_word_count[file];
+            tf = freq / dataset.corpus_word_count[file];
+            tf_idf = idf * tf;
+
+            // modify weighted_terms object
+            if (word in weightedTerms) {
+              weightedTerms[word].push({ [file]: tf_idf });
+            } else {
+              weightedTerms[word] = [{ [file]: tf_idf }];
+            }
+          });
+        });
+
+        // output to file
+        const jsonString = JSON.stringify(weightedTerms, null, 2);
+
+        try {
+          fs.writeFileSync(weightedTermsPath, jsonString);
+          console.log(`Indexed terms successfully weighted`);
+        } catch (error) {
+          console.log("Weighted terms creation failed", error);
+        }
+      } else if (typeOfIndex === "query") {
+        let tf = 0;
+        let idf = 1;
+        let tf_idf = 0;
+
+        // calculate length normalized term  frequency and inverse document frewuency
+        Object.keys(dataset.words).forEach((word) => {
+          let freq = dataset.words[word];
+
+          tf = freq / dataset.corpus_word_count;
           tf_idf = idf * tf;
 
-          // modify weighted_terms object
-          if (word in weightedTerms) {
-            weightedTerms[word].push({ [file]: tf_idf });
-          } else {
-            weightedTerms[word] = [{ [file]: tf_idf }];
-          }
+          weightedTerms[word] = tf_idf;
         });
-      });
 
-      // output to file
-      const jsonString = JSON.stringify(weightedTerms, null, 2);
+        // output to file
+        const jsonString = JSON.stringify(weightedTerms, null, 2);
 
-      try {
-        fs.writeFileSync(weightedTermsPath, jsonString);
-        console.log(`Indexed terms successfully weighted`);
-      } catch (error) {
-        console.log("Weighted terms creation failed", error);
+        try {
+          fs.writeFileSync(weightedTermsPath, jsonString);
+          console.log(`Indexed terms successfully weighted`);
+        } catch (error) {
+          console.log("Weighted terms creation failed", error);
+        }
       }
     } catch (err) {
       console.log("Error parsing JSON string:", err);
@@ -50,7 +76,5 @@ function weigh_terms(indexFilePath, outputWeightedTermsPath) {
     console.log(`Error reading Index file ${indexFilePath} from disk:`, error);
   }
 }
-
-weigh_terms("./indexFile.json", "./");
 
 export default weigh_terms;
